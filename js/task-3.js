b16f6c810d45039a53f7833104474b66f2ce7a19
Max Moroz <maksim.moroz@gmail.com>
2024-04-01 23:55:14 +0100

    up

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function getElementWidth(content, padding, border) {
+    content = Number.parseFloat(content);
+    padding = Number.parseFloat(padding);
+    border = Number.parseFloat(border);
+    
+    totalWidth = content + 2 * padding + 2 * border;
+    return totalWidth;
+ }
+
+getElementWidth();
+
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
