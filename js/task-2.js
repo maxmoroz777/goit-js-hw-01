5f55c10e3a040b57b18baca33921c705a514150c
Max Moroz <maksim.moroz@gmail.com>
2024-04-23 11:38:50 +0100

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,6 @@
 function getShippingMessage(country, price, deliveryFee) {
-    totalPrice = price + deliveryFee;
-    result = `Shipping to ${country} will cost ${totalPrice} credits`;
+   let totalPrice = price + deliveryFee;
+   let result = `Shipping to ${country} will cost ${totalPrice} credits`;
     return result;
 }
 
